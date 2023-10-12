9a074d5cfd4b67fbd6839517411e3f149d0d04dd
Robin Blondin <robin.blondin@protonmail.com>
2023-10-12 21:58:40 +0200

    Added values and a getter to the constants

--- src/Membership.java
+++ src/Membership.java
@@ -1,5 +1,15 @@
 public enum Membership {
-    ACTIVE,
-    EXPIRED,
-    NONE
+    ACTIVE("has an active subscription."),
+    EXPIRED("has an expired subscription."),
+    NONE("is not a member.");
+
+    private final String status;
+
+    Membership(String status) {
+        this.status = status;
+    }
+
+    public String getStatus() {
+        return status;
+    }
 }
