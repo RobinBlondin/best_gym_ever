68489ebf4c43407c52bf5b3c8875bf7b4395bda2
Robin Blondin <robin.blondin@protonmail.com>
2023-10-12 23:49:58 +0200

    Created methods based on tests

--- src/ReadFile.java
+++ src/ReadFile.java
@@ -1,23 +1,44 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.time.LocalDate;
+import java.time.format.DateTimeParseException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class ReadFile {
-    private boolean isTest;
-    private String path;
-
-    //region Constructors
-    public ReadFile() {}
-
-    public ReadFile(boolean isTest, String path) {
-        this.isTest = isTest;
-        this.path = path;
+    protected List<Customer> readFileToList(boolean isTest, String path) throws IOException {
+        List<Customer> result = new ArrayList<>();
+        try(BufferedReader br = new BufferedReader(new FileReader(path))) {
+            String line;
+            while((line = br.readLine()) != null) {
+                result.add(new Customer(parseName(line), parseSocialNumber(line), parseDate(br.readLine())));
+            }
+        } catch(IOException e) {
+            if(isTest) {
+                throw e;
+            } else {
+                System.out.println("File not found!");
+            }
+        } catch(DateTimeParseException e) {
+            if(isTest) {
+                throw e;
+            } else {
+                System.out.println("parseDate couldn't parse String to LocalDate");
+            }
+        }
+        return result;
     }
-    //endregion
 
-    //region Getters and setters
-    public String getPath() {
-        return path;
+    protected String parseName(String line) {
+        return line.split(", ")[1];
     }
 
-    public void setPath(String path) {
-        this.path = path;
+    protected String parseSocialNumber(String line) {
+        return line.split(", ")[0];
     }
-    //endregion
+
+    protected LocalDate parseDate(String line) {
+        return LocalDate.parse(line);
+    }
 }
