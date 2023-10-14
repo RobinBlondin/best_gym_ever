c7ae7586dd56a437c6e334180dd738042dbad2b1
Robin Blondin <robin.blondin@protonmail.com>
2023-10-14 07:28:05 +0200

    Changed name of boolean

--- src/BestGymEver.java
+++ src/BestGymEver.java
@@ -2,13 +2,17 @@ import java.util.List;
 import java.util.Scanner;
 
 public class BestGymEver {
-    private final boolean isTest;
-    private List<Customer> customers;
+    private final boolean testMode;
+    private final List<Customer> customers;
     private final Scanner sc;
 
-    public BestGymEver(boolean isTest) {
-        this.isTest = isTest;
+    public BestGymEver(boolean testMode) throws Exception {
+        String inputData = "./files/customers.txt";
+        String outputData = "./files/log.txt";
         this.testMode = testMode;
+
+        readFile = new ReadFile(false, inputData);
+        writeFile = new WriteFile(false, outputData);
         sc = new Scanner(System.in);
     }
 
