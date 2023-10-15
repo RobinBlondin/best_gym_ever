a7e8540e0f4a16e45036b7eadf0fdd6c2244444c
Robin Blondin <robin.blondin@protonmail.com>
2023-10-15 08:04:20 +0200

    Added tests for setSubscription method and move testobjects to global scope.

--- test/CustomerTest.java
+++ test/CustomerTest.java
@@ -5,18 +5,26 @@ import java.time.LocalDate;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class CustomerTest {
-
+    Customer lessThanYear = new Customer("", "", LocalDate.of(2023, 6, 25));
+    Customer exactlyOneYear = new Customer("", "", LocalDate.now().minusYears(1));
+    Customer moreThanYear = new Customer("", "", LocalDate.of(2022, 6, 25));
 
 
     @Test
     public void isSubscriber() {
-        Customer lessThanYear = new Customer("", "", LocalDate.of(2023, 6, 25));
-        Customer exactlyOneYear = new Customer("", "", LocalDate.now().minusYears(1));
-        Customer moreThanYear = new Customer("", "", LocalDate.of(2022, 6, 25));
+        assertTrue(lessThanYear.isSubscriber());
+        assertTrue(exactlyOneYear.isSubscriber());
+        assertFalse(moreThanYear.isSubscriber());
+    }
 
-        assertEquals(1, lessThanYear.isSubscriber());
-        assertEquals(1, exactlyOneYear.isSubscriber());
-        assertEquals(0, moreThanYear.isSubscriber());
+    @Test
+    public void setSubscription() {
+        Subscription expectedActive = Subscription.ACTIVE;
+        Subscription expectedExpired = Subscription.EXPIRED;
+
+        assertEquals(expectedActive, lessThanYear.getSubscription());
+        assertEquals(expectedActive, exactlyOneYear.getSubscription());
+        assertEquals(expectedExpired, moreThanYear.getSubscription());
     }
 
 }
