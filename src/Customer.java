54e831a36735f124b7630cb96d4c619cd211bb71
Robin Blondin <robin.blondin@protonmail.com>
2023-10-15 08:42:13 +0200

    Changed isSubscriber to return boolean.
    Created a setter that uses the isSubscriber to set the subscription variable upon instantiation.

--- src/Customer.java
+++ src/Customer.java
@@ -2,12 +2,14 @@ import java.time.LocalDate;
 
 public class Customer extends Person {
     private LocalDate joinDate;
+    private Subscription subscription;
 
     public Customer(){}
 
     public Customer(String name, String socialSecurityNumber, LocalDate joinDate) {
         super(name, socialSecurityNumber);
         this.joinDate = joinDate;
+        setSubscription();
     }
 
     //region Getter and setters
@@ -18,16 +20,23 @@ public class Customer extends Person {
     public void setJoinDate(LocalDate joinDate) {
         this.joinDate = joinDate;
     }
+
+    public Subscription getSubscription() {
+        return subscription;
+    }
+
+    public void setSubscription() {
+        if (isSubscriber()) {
+            this.subscription = Subscription.ACTIVE;
+        } else {
+            this.subscription = Subscription.EXPIRED;
+        }
+
+    }
     //endregion
 
-    protected int isSubscriber(){
-        boolean isSubscriber = !(joinDate.plusYears(1).isBefore(LocalDate.now()));
-
-        if(isSubscriber) {
-            return 1;
-        } else {
-            return 0;
-        }
+    protected boolean isSubscriber(){
+        return !(joinDate.plusYears(1).isBefore(LocalDate.now()));
     }
 
 }
