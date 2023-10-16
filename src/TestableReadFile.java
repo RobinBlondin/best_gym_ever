c017a92dd4bd42aec225d27f56fdfb34a2005c1d
Robin Blondin <robin.blondin@protonmail.com>
2023-10-16 14:16:06 +0200

    Removed interfaces

--- src/TestableReadFile.java
+++
@@ -1,8 +0,0 @@
-import java.io.IOException;
-import java.time.format.DateTimeParseException;
-import java.util.List;
-
-public interface TestableReadFile {
-    List<Customer> readFileToList() throws IOException, DateTimeParseException;
-
-}
--- src/TestableWriteFile.java
+++
@@ -1,5 +0,0 @@
-import java.io.IOException;
-
-public interface TestableWriteFile {
-    void logEntry(String entry) throws IOException;
-}
