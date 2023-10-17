acf83d711f269fa3b65b2164ae8a21a2b1770cb0
Robin Blondin <robin.blondin@protonmail.com>
2023-10-17 20:08:15 +0200

    replaced hard coded values with constants

--- src/ReadFile.java
+++ src/ReadFile.java
@@ -7,6 +7,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ReadFile extends File {
+    Constants c = new Constants();
     public ReadFile(boolean testMode, String path) {
         super(testMode, path);
     }
