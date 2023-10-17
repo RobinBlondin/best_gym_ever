bc3d238f2956098f9dd473e4ca5b78cd8e257a58
Robin Blondin <robin.blondin@protonmail.com>
2023-10-17 20:07:06 +0200

    Added more test cases to parse methods

--- test/ReadFileTest.java
+++ test/ReadFileTest.java
@@ -34,29 +34,36 @@ public class ReadFileTest{
 
     @Test
     public void parseName() {
-        String line = "12345677890, firstname lastname";
-        String expected = "firstname lastname";
+        String line1 = "1234567890, firstname lastname";
+        String line2 = "1234567890, firstname";
+        String expected1 = "firstname lastname";
+        String expected2 = "firstname";
 
-        assertEquals(expected, r.parseName(line));
-        assertNotEquals(line, r.parseName(line));
+        assertEquals(expected1, validPath.parseName(line1));
+        assertEquals(expected2, validPath.parseName(line2));
+        assertNotEquals(line1, validPath.parseName(line1));
     }
 
     @Test
     public void parseSocialNumber() {
-        String line = "12345677890, firstname lastname";
-        String expected = "12345677890";
+        String validNumber1 = "1234567890, firstname lastname";
+        String validNumber2 = "123456789012, firstname lastname";
+        String expected1 = "1234567890";
+        String expected2 = "123456789012";
 
-        assertEquals(expected, r.parseSocialNumber(line));
-        assertNotEquals(line, r.parseSocialNumber(line));
+        assertEquals(expected1, validPath.parseSocialNumber(validNumber1));
+        assertEquals(expected2, validPath.parseSocialNumber(validNumber2));
+        assertNotEquals(validNumber1, validPath.parseSocialNumber(validNumber1));
+        assertNotEquals(validNumber2, validPath.parseSocialNumber(validNumber2));
     }
 
     @Test
     public void parseDate() {
         String line = "2014-03-26";
-        String wrongInput = "2345234rt53";
+        String invalidDate = "2345234rt53";
         LocalDate expected = LocalDate.of(2014, 3, 26);
 
-        assertEquals(expected, r.parseDate(line));
-        assertThrows(DateTimeParseException.class, () -> r.parseDate(wrongInput));
+        assertEquals(expected, validPath.parseDate(line));
+        assertThrows(DateTimeParseException.class, () -> validPath.parseDate(invalidDate));
     }
 }
