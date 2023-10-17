8af7694a086dbac4dbb05841cb13c8b7111f150d
Robin Blondin <robin.blondin@protonmail.com>
2023-10-17 20:09:41 +0200

    Removed instantiation of File before making it abstract

--- test/FileTest.java
+++ test/FileTest.java
@@ -8,14 +8,12 @@ class FileTest {
 
     @Test
     void errorMessage() {
-        File file = new File(true, "");
+        ReadFile file = new ReadFile(true, "");
 
         Exception IO = new IOException();
         Exception nullP = new NullPointerException();
 
         assertThrows(IO.getClass(), () -> file.errorMessage(IO, ""));
         assertThrows(nullP.getClass(), () -> file.errorMessage(nullP, ""));
-        //assertThrows()
-
     }
 }
